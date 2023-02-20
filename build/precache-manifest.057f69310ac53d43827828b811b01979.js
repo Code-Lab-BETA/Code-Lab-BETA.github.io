0a8bae8c90b0407fb39ac9fe8673e68f54e33663
chenpotatos <103394020+chenpotatos@users.noreply.github.com>
2023-02-20 15:28:20 +0800

    Add files via upload

---
+++ build/precache-manifest.057f69310ac53d43827828b811b01979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb23f5ef8345d4d60ffb7ae1f495566f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d75f385e8dd7dadcf52",
+    "url": "/static/css/main.70c9aa49.chunk.css"
+  },
+  {
+    "revision": "b4938b36a96938db3094",
+    "url": "/static/js/2.b2c97919.chunk.js"
+  },
+  {
+    "revision": "9d75f385e8dd7dadcf52",
+    "url": "/static/js/main.007a4400.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
